b44e7d380270609bf41bb60fa6bbb69cc788078d
Sebastian Rosales <srosales.dev@gmail.com>
2023-07-15 02:39:54 -0300

    Solo se reemplaza un archivo de estilos.

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 function ItemListContainer({ greeting }) {
   return (
     <Container fluid>
-      <h1 className="pt-4 text-center">{greeting}</h1>
+      <h1 className="pt-4 text-center">{ greeting }</h1>
     </Container>
   );
 }
